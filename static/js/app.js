31271745069cae53e0ffd9aef75eb3f50f90197f
ravendcode <ravendcode@gmail.com>
2018-03-13 23:17:56 +0200

    Fix

--- static/js/app.js
+++ static/js/app.js
@@ -28,23 +28,22 @@
       var liElem = document.createElement('li');
       var content = document.createTextNode(received.uuid + ': ' + received.data);
       if (received.type === 'server') {
-        if (!uuid) {
-          var uuidTextNode = document.createTextNode(received.uuid);
-          uuidElem.appendChild(uuidTextNode)
-          content = document.createTextNode(received.type + ': ' + received.data);
-        }
         uuid = received.uuid;
+
+        var uuidTextNode = document.createTextNode(received.uuid);
+        uuidElem.appendChild(uuidTextNode)
+        content = document.createTextNode(received.type + ': ' + received.data);
       }
       liElem.appendChild(content);
       messagesUlElem.appendChild(liElem);
     };
 
     echoWs.onclose = function (event) {
-      // console.log('Socket is closed. Reconnect will be attempted in 1 second.');
+      console.log('Socket is closed. Reconnect will be attempted in 1 second.');
       // setTimeout(function () {
-        //   connect();
-        // }, 1000);
-      };
+      //   connect();
+      // }, 1000);
+    };
 
     messageFormElem.onsubmit = function (event) {
       event.preventDefault();
